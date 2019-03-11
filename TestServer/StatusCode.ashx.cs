c92639ec8763ab1644a608f10a8acdc07c44f2d1
David Shulman <david.shulman@microsoft.com>
2019-03-11 13:12:47 -0700

    Add StatusCode support

---
+++ TestServer/StatusCode.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+
+namespace TestServer
+{
+    public class StatusCode : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string statusCodeString = context.Request.QueryString["statuscode"];
+            string statusDescription = context.Request.QueryString["statusdescription"];
+            try
+            {
+                int statusCode = int.Parse(statusCodeString);
+                context.Response.StatusCode = statusCode;
+                if (statusDescription != null)
+                {
+                    context.Response.StatusDescription = statusDescription;
+                }
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.StatusDescription = "Error parsing statuscode: " + statusCodeString;
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
